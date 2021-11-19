dca145adc4e512f6666576dacd02644377fc709e
k000 <kintore@mineo.jp>
2021-11-19 23:02:23 +0900

    クラス分割など考えず処理だけ記載 コマンドでインターフェースと実装クラスおよび指定フォルダの作成ができるまで確認

--- app/Console/Commands/InsertDataCommand.php
+++ app/Console/Commands/InsertDataCommand.php
@@ -5,7 +5,6 @@ namespace App\Console\Commands;
 use Exception;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
-use Symfony\Component\Translation\Loader\YamlFileLoader;
 
 class InsertDataCommand extends Command
 {
